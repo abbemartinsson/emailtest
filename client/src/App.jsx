28f718233b047447327161e4e1e9e6d1e77852df
Albin Martinsson <albinmartinsson@MacBook-Air-som-tillhor-Albin.local>
2025-02-10 18:30:53 +0100

    first try

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import emailjs from '@emailjs/browser'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+        
+        emailjs.sendForm('service_uj9e0x4', 'template_bkfchwz', e.target, 'oC-Q7nem3JATe04Mh');
+    }
+ return(
+     <div className="App">
+         <h1 className="pageTitle">Email test!</h1>
+         <form className="contactform" onSubmit={sendEmail}>
+             <label htmlFor="emailFrom">Email From:</label>
+             <input type="text" id="emailFrom" name="emailFrom" className="emailFrom"/>
+             <label htmlFor="emailTo">Send Email to:</label>
+             <input type="text" name="emailTo" id="emailTo" className=emailTo/>
+             <label htmlFor="message">Message:</label>
+             <textarea name="message" id="message" className="messageBox"></textarea>
+             <button type="submit" variant="contained" className="submit">Submit</button>
+         </form>
+     </div>
+ )
 }
 
 export default App
